29871f9f75e1358b5307b814369be80838b5851a
whirlwind88 <1988valeron@gmail.com>
2022-04-06 21:16:56 +0300

    2я задача

---
+++ Lesson3_Task2.py
@@ -0,0 +1,8 @@
+text1 = input("введите первое число \n")
+value1 = int(text1)
+text2 = input("введите первое число \n")
+value2 = int(text2)
+text3 = input("введите первое число \n")
+value3 = int(text3)
+average = (value1+value2+value3)/3
+print("{0:.3f}".format(average))
