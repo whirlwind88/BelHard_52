6b42351500fb1ede57f1dff49aa5f92575181a15
whirlwind88 <1988valeron@gmail.com>
2022-04-06 21:50:39 +0300

    3я задача(2 способ)

---
+++ Lesson3_Task3(2).py
@@ -0,0 +1,4 @@
+first_name = input("Ваше Имя- \n")
+age = input("Ваш возраст- \n")
+town = input("Ваш город - \n")
+print("меня зовут {}. Мне - {}. Я живу в городе {}".format(first_name, age, town))
