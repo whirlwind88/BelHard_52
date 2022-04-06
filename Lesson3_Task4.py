167e63a3cc27c7862a52b284ed92f28a9a38356a
whirlwind88 <1988valeron@gmail.com>
2022-04-06 22:44:22 +0300

    3 способ

---
+++ Lesson3_Task4.py
@@ -0,0 +1,9 @@
+text1 = input("первое число- \n")
+text2 = input("второе число- \n")
+text3 = input("третье число- \n")
+print()
+text = text1 + text2 + text3
+print("количество положительных чисел равно=", 3-text.count('-'))
+print("количество отрицательных чисел равно=", text.count('-'))
+
+
