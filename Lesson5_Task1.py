34ff6699cb6513df42cd6cef6e42da03530e1942
whirlwind88 <1988valeron@gmail.com>
2022-04-13 21:08:13 +0300

    вывод первых Nчисел

---
+++ Lesson5_Task1.py
@@ -0,0 +1,13 @@
+#вывести первые N чисел кратные M и большие K
+
+#import sys
+
+print('введите число M')
+m = int(input())
+print('введите число K')
+k = int(input())
+print ('введите ограничение на количество выводимых чисел')
+n = int(input())
+for i in range(k+n*m):
+    if i % m == 0 and i > k:
+        print(i, end=' ')
